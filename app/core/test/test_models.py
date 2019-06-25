4125842ca69e8663b86475365fa776309e4147d4
Ornella Ramos <ornella.ramos.velandia@gmail.com>
2019-06-25 11:54:29 -0500

    Added recipe model

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -61,3 +61,14 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test de recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Pasta Carbonara',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
