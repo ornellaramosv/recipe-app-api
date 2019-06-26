786b1409d5ad133977bb75c5e97ac1c768b11980
Ornella Ramos <ornella.ramos.velandia@gmail.com>
2019-06-26 17:05:17 -0500

    Added list recipe feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -6,6 +6,7 @@ from recipe import views
 router = DefaultRouter()
 router.register('tag', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = 'recipe'
 
